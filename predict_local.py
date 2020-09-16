71ecd7e2b535dda053e4159263b21d1a74a33f6d
Shinya Kinjo <65533712+mokoshin0720@users.noreply.github.com>
2020-09-17 01:19:17 +0900

    コメントの追加

--- predict_local.py
+++ predict_local.py
@@ -1,3 +1,6 @@
+# ターミナルで動かすためのファイル
+# predict_file.pyとほとんど同じ内容
+
 from keras.models import Sequential, Model, load_model
 from keras.layers import Conv2D, MaxPooling2D
 from keras.layers import Input, Activation, Dropout, Flatten, Dense
@@ -16,26 +19,21 @@ img_size = 50
 cascade_path = "face.xml"
 faceCascade = cv2.CascadeClassifier(cascade_path)
 
-def build_model():
-    # VGG16のロード。FC層は不要なので include_top=False
+def build_model(): # モデルの構築 → obama_tuning.pyとほぼ同じ
     input_tensor = Input(shape=(img_size, img_size, 3))
     vgg16 = VGG16(include_top=False, weights='imagenet', input_tensor=input_tensor)
 
-    # VGG16の図の緑色の部分（FC層）の作成
     top_model = Sequential()
     top_model.add(Flatten(input_shape=vgg16.output_shape[1:]))
     top_model.add(Dense(256, activation='relu'))
     top_model.add(Dropout(0.5))
     top_model.add(Dense(num_names, activation='softmax'))
 
-    # VGG16とFC層を結合してモデルを作成（完成図が上の図）
     vgg_model = Model(inputs=vgg16.input, outputs=top_model(vgg16.output))
 
-    # VGG16の図の青色の部分は重みを固定（frozen）
     for layer in vgg_model.layers[:15]:
         layer.trainable = False
 
-    # 多クラス分類を指定
     vgg_model.compile(loss='categorical_crossentropy',
             optimizer="sgd",
             metrics=['accuracy'])
@@ -45,11 +43,11 @@ def build_model():
     return vgg_model
 
 def main():
-    img = cv2.imread(sys.argv[1], cv2.IMREAD_COLOR)
+    img = cv2.imread(sys.argv[1], cv2.IMREAD_COLOR) # 画像をimgに格納
 
-    if img is None:
+    if img is None: # 画像がない場合
         print(sys.argv[1], "は顔を検出できませんでした。")
-    else:
+    else: # 顔を切り取る処理
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         face = faceCascade.detectMultiScale(gray, 1.1, 3)
 
@@ -65,9 +63,10 @@ def main():
                 img = img[y:y+h, x:x+w]
                 cv2.imwrite("./" + sys.argv[2] + "_face.jpg", img)
 
-        else:
+        else: # 顔が読み取れない場合
             print(sys.argv[1],  "は顔を検出できませんでした。")
 
+    # 切り取れた顔画像をnumpy配列に変換 → dataに格納
     img_test = Image.open("./" + sys.argv[2] + "_face.jpg")
     img_test = img_test.convert("RGB")
     img_test = img_test.resize((img_size, img_size))
@@ -77,14 +76,14 @@ def main():
     X.append(data)
     X = np.array(X)
     model = build_model()
-
+    
+    # 画像の予想
     result = model.predict([X])[0]
     print("result:", result)
     predicted = result.argmax()
     print(predicted)
     percentage = int(result[predicted] * 100)
-    print("{0}, {1}%".format(names[predicted], percentage))
-    print(result, "\nオバマ、ノッチ")
+    print(percentage + "%" + predicted[name] + "でしょう！")
 
 if __name__ == "__main__":
     main()
